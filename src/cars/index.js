48b27fcece61194eea98767e5a9ec919574a4f04
vasa-hadzheha <73876508+vasa-hadzheha@users.noreply.github.com>
2021-12-06 21:33:17 +0200

    add proect

---
+++ src/cars/index.js
@@ -0,0 +1,13 @@
+const Router = require("express").Router;
+const carController = require("./controller_db.js");
+
+const carRouter = new Router();
+
+carRouter.get("/", carController.getAll);
+carRouter.get("/query", carController.getQuery);
+carRouter.get("/:id", carController.getById);
+carRouter.delete("/:id", carController.delete);
+carRouter.post("/", carController.post);
+carRouter.patch("/:id", carController.patch);
+
+module.exports = carRouter;
