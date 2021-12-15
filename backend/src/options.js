ff16b46e14a60338b7ba1cbf31a6821974249b5d
vasa-hadzheha <73876508+vasa-hadzheha@users.noreply.github.com>
2021-12-15 16:39:23 +0200

    add one more dbPath for Birthdays database

--- backend/src/options.js
+++ backend/src/options.js
@@ -3,6 +3,7 @@ module.exports = {
     fileName: "./data/Car.json",
     retryInterval: 1000,
     dbPath: "./data/Cars.db",
+    dbPathBirth: "./data/Birthdays.db",
     frontendPort: 5000,
     frontendIP: "127.0.0.42",
 }
